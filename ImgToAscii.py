0e198500bfd159b0b943728e98046688bf14d12b
eloBro <36836868+ehlwas@users.noreply.github.com>
2021-11-29 05:16:13 +0800

    Add files via upload

---
+++ ImgToAscii.py
@@ -0,0 +1,43 @@
+import tkinter as tk
+from PIL import ImageTk, Image
+import PIL
+
+ASCII_CHARS = ["@", "#", "S", "%", "?", "*", "+", ";", ":", ",", "."]
+
+def resize_image(image, new_width=100):
+    width, height = image.size
+    ratio = height/width
+    new_height = int(new_width * ratio)
+    resized_image = image.resize((new_width, new_height-50))
+    return(resized_image)
+
+def grayify(image):
+    grayscale_image = image.convert("L")
+    return(grayscale_image)
+    
+def pixels_to_ascii(image):
+    pixels = image.getdata()
+    characters = "".join([ASCII_CHARS[pixel//25] for pixel in pixels])
+    return(characters)    
+
+def main(new_width=100):
+    path = input("Enter a valid pathname to an image:\n")
+    try:
+        image = PIL.Image.open(path)
+    except:
+        print(path, " is not a valid pathname to an image.")
+        return
+  
+    new_image_data = pixels_to_ascii(grayify(resize_image(image)))
+    
+    pixel_count = len(new_image_data)  
+    ascii_image = "\n".join([new_image_data[index:(index+new_width)] for index in range(0, pixel_count, new_width)])
+    
+    print(ascii_image)
+    
+    with open("etona.txt", "w") as f:
+        f.write(ascii_image)
+ 
+main()
+
+input()
